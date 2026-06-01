318b6857582c5a5b7e2a55af8a53cd61a6c9395e
Misha Levchenkov <mihalevch@mail.ru>
2026-06-01 15:37:09 +0300

    signature modified

--- abstract_object.py
+++ abstract_object.py
@@ -20,11 +20,27 @@ class CachableObject(AbstractObject):
     
     signature(self) - создание сигнатуры для хэш-таблицы
     """
+    @staticmethod
+    def _normalize_signature_value(value):
+        if isinstance(value, float):
+            return round(value, 12)
+        return value
+
     def signature(self):
+        cache_signature_fields = getattr(self, "cache_signature_fields", None)
+        if cache_signature_fields is not None:
+            return tuple(
+                (
+                    field_name,
+                    self._normalize_signature_value(getattr(self, field_name)),
+                )
+                for field_name in cache_signature_fields
+            )
+
         signature_list = []
         for item in self.__dict__.values():
             if isinstance(item, typing.Hashable):
-                signature_list.append(item)
+                signature_list.append(self._normalize_signature_value(item))
         return tuple(signature_list)
 
 class AbstractJsonableObject(AbstractObject):
